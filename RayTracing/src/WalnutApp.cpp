4f32c0153ff5a98f26ea77c626689ced97b4c7c8
TessellatedIRL <shantanuy2kandroid@gmail.com>
2023-01-01 19:52:54 +0530

    Initial commit

---
+++ RayTracing/src/WalnutApp.cpp
@@ -0,0 +1,148 @@
+#include "Walnut/Application.h"
+#include "Walnut/EntryPoint.h"
+
+#include "Walnut/Image.h"
+#include "Walnut/Timer.h"
+#include"glm/gtc/type_ptr.hpp"
+
+#include "Renderer.h"
+
+using namespace Walnut;
+
+class ExampleLayer : public Walnut::Layer
+{
+public:
+	ExampleLayer()
+		:m_camera(45.0f, 1.0f, 100.0f)
+	{
+		Material& SubjectSphere = m_scene.Materials.emplace_back();
+		SubjectSphere.Albedo = { 1.0f, 0.0f, 1.0f };
+		SubjectSphere.Roughness = 0.0f;
+		SubjectSphere.Metallic = 0.0f;
+		
+		Material& GroundSphere = m_scene.Materials.emplace_back();
+		GroundSphere.Albedo = { 0.2f, 0.3f, 1.0f };
+		GroundSphere.Roughness = 0.1f;
+		GroundSphere.Metallic = 0.0f;
+		
+		{
+			Sphere sphere;
+			sphere.Position = { 0.0f, 0.0f, 0.0f };
+			sphere.Radius = 1.0f;
+			sphere.MaterialIndex = 0;
+			m_scene.Spheres.push_back(sphere);
+		}
+		{
+			Sphere sphere;
+			sphere.Position = { 0.0f, -101.0f, 0.0f };
+			sphere.Radius = 100.0f;
+			sphere.MaterialIndex = 1;
+			m_scene.Spheres.push_back(sphere);
+		}
+	}
+
+	virtual void OnUpdate(float ts) override
+	{
+		if (m_camera.OnUpdate(ts))
+			m_Renderer.ResetFrameIndex();
+	}
+	
+	virtual void OnUIRender() override
+	{
+		ImGui::Begin("Settings");
+		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
+		if (ImGui::Button("Render"))
+		{
+			Render();
+		}
+		
+		ImGui::Checkbox("Accumulate", &m_Renderer.GetSettings().Accumulate);
+		
+		if (ImGui::Button("Reset"))
+			m_Renderer.ResetFrameIndex();
+		
+		ImGui::End();
+
+		ImGui::Begin("Scene");
+		for (size_t i = 0; i < m_scene.Spheres.size(); i++)
+		{
+			ImGui::PushID(i);
+			
+			Sphere& sphere = m_scene.Spheres[i];
+			ImGui::DragFloat3("Position", glm::value_ptr(sphere.Position), 0.1f);
+			ImGui::DragFloat("Radius", &sphere.Radius, 0.1f);
+			ImGui::DragInt("Material", &sphere.MaterialIndex, 1, 0, (int)m_scene.Spheres.size() - 1);
+
+			ImGui::Separator();
+			ImGui::PopID();
+		}
+		for (size_t i = 0; i < m_scene.Spheres.size(); i++)
+		{
+			ImGui::PushID(i);
+			
+			Material& material = m_scene.Materials[i];
+			ImGui::ColorEdit3("Albedo", glm::value_ptr(material.Albedo));
+			ImGui::DragFloat("Roughness", &material.Roughness, 0.05f);
+			ImGui::DragFloat("Metallic", &material.Metallic, 0.05f);
+
+			ImGui::Separator();
+			ImGui::PopID();
+		}
+		ImGui::End();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("Viewport");
+
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() }, ImVec2(0, 1), ImVec2(1, 0));
+
+		ImGui::End();
+		ImGui::PopStyleVar();
+
+		Render();
+	}
+
+	void Render()
+	{
+		Timer timer;
+
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_camera.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render(m_scene, m_camera);
+
+		m_LastRenderTime = timer.ElapsedMillis();
+	}
+
+private:
+	Renderer m_Renderer;
+	Camera m_camera;
+	Scene m_scene;
+	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+
+	float m_LastRenderTime = 0.0f;
+};
+
+Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+{
+	Walnut::ApplicationSpecification spec;
+	spec.Name = "RayTracing";
+
+	Walnut::Application* app = new Walnut::Application(spec);
+	app->PushLayer<ExampleLayer>();
+	app->SetMenubarCallback([app]()
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("Exit"))
+			{
+				app->Close();
+			}
+			ImGui::EndMenu();
+		}
+	});
+	return app;
+}
